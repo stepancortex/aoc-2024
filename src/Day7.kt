99e85363b7371328b4653ba959cca5d96118590d
Stepan Grigoryan <stepan.grigoryan@cortex.io>
2024-12-07 09:40:38 -0800

    make day7 solution faster

--- src/Day7.kt
+++ src/Day7.kt
@@ -1,6 +1,9 @@
 package day7
 
 import kotlin.io.path.Path
+import kotlin.math.floor
+import kotlin.math.log10
+import kotlin.math.pow
 
 enum class Operation {
     ADD,
@@ -11,7 +14,10 @@ enum class Operation {
         return when (this) {
             ADD -> a + b
             MULTIPLY -> a * b
-            CONCAT -> "${a}${b}".toLong()
+            CONCAT -> {
+                val digits = floor(log10(b.toDouble())).toInt() + 1
+                return a * 10.0.pow(digits).toLong() + b
+            }
         }
     }
 }
@@ -20,38 +26,38 @@ fun getInput() =
     Path("resources/Day7.txt").toFile().readText().split("\n").map {
         val parts = it.split(":")
         val nums = parts[1].split(" ").mapNotNull { num -> num.toLongOrNull() }
-        return@map Pair<Long, List<Long>>(parts[0].toLong(), nums)
+        return@map Pair<Long, ArrayDeque<Long>>(parts[0].toLong(), ArrayDeque(nums))
     }
 
-fun p1(input: List<Pair<Long, List<Long>>>): Long {
+fun p1(input: List<Pair<Long, ArrayDeque<Long>>>): Long {
     return helper(input, listOf(Operation.MULTIPLY, Operation.ADD))
 }
 
-fun p2(input: List<Pair<Long, List<Long>>>): Long {
+fun p2(input: List<Pair<Long, ArrayDeque<Long>>>): Long {
     return helper(input, listOf(Operation.CONCAT, Operation.MULTIPLY, Operation.ADD))
 }
 
-fun helper(input: List<Pair<Long, List<Long>>>, operations: List<Operation>): Long {
+fun helper(input: List<Pair<Long, ArrayDeque<Long>>>, operations: List<Operation>): Long {
     return input.filter { evaluate(it.first, it.second, operations) }.sumOf { it.first }
 }
 
-fun evaluate(number: Long, equation: List<Long>, operations: List<Operation>): Boolean {
+fun evaluate(number: Long, equation: ArrayDeque<Long>, operations: List<Operation>): Boolean {
     if (equation.size == 1) {
         return number == equation[0]
     }
-    val operationsCopy = operations.toMutableList()
-    while (operationsCopy.isNotEmpty()) {
-        val equationCopy = equation.toMutableList()
-        val operation = operationsCopy.removeAt(0)
-        val a = equationCopy.removeAt(0)
-        val b = equationCopy.removeAt(0)
+    operations.forEach { operation ->
+        val a = equation.removeFirst()
+        val b = equation.removeFirst()
         val result = operation.execute(a, b)
         if (result <= number) {
-            equationCopy.add(0, result)
-            if (evaluate(number, equationCopy, operations)) {
+            equation.addFirst(result)
+            if (evaluate(number, equation, operations)) {
                 return true
             }
+            equation.removeFirst()
         }
+        equation.addFirst(b)
+        equation.addFirst(a)
     }
 
     return false
